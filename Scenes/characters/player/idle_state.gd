4321198e9573959c2fcda50ed8ebcda9aee3d840
EeebsieKat <eeebsthemodcat@gmail.com>
2025-12-27 12:45:05 -0700

    Added basic topdown stuffs

---
+++ Scenes/characters/player/idle_state.gd
@@ -0,0 +1,36 @@
+extends NodeState
+
+@export var player: CharacterBody2D
+@export var animated_sprite_2d: AnimatedSprite2D
+
+var direction: Vector2
+
+func _on_process(_delta : float) -> void:
+	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	if Input.is_action_pressed("walk_left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_pressed("walk_right"):
+		direction = Vector2.RIGHT
+	elif Input.is_action_pressed("walk_up"):
+		direction = Vector2.UP
+	elif Input.is_action_pressed("walk_down"):
+		direction = Vector2.DOWN
+	else:
+		direction = Vector2.ZERO
+		
+	
+
+
+func _on_next_transitions() -> void:
+	pass
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	pass
